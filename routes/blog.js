0d675a069509ceca97cbbeb6cf078ca7491b5d72
Sankalp Panchabhai <147154199+sankalppanchabhai123@users.noreply.github.com>
2025-08-11 23:32:41 +0530

    Adding Multer to upload files

--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,20 @@
 const {Router}=require("express");
 const multer=require("multer")
+const path=require("path")
 const Blog = require("../modules/blog");
 const router= Router();
+
+const storage=multer.diskStorage({
+    destination: function (req,file,cb){
+        cb(null, path.resolve(`./public/uploads/`))
+    },
+    filename: function (req,file,cb){
+        const fileName=`${Date.now()}-${file.originalname}`;
+        cb(null, fileName);
+    },
+})
+
+const upload=multer({storage: storage})
 
 router.get("/add-new",(req,res)=>{
     return res.render("addblog",{
@@ -9,13 +22,10 @@ router.get("/add-new",(req,res)=>{
     })
 })
 
-router.post("/add-new",async (req,res)=>{
-    const { title, body, coverImageURL }=req.body;
-    await Blog.create({
-        title,
-        body,
-        coverImageURL,
-    })
+router.post("/add-new",upload.single("coverImage"), (req,res)=>{
+    // const { title, body, coverImageURL }=req.body;
+    console.log(req.body);
+    console.log(req.file);
     return res.redirect("/");
 })
 
